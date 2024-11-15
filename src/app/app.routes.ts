b7f0d82e093e2d202336ac55bb5f650343f3c986
nidhal <nidhalben13@gmail.com>
2024-11-15 10:36:14 +0100

    login signup interfaces done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'login',component:LoginComponent},
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    {path:'signup',component:SignupComponent}
+];
