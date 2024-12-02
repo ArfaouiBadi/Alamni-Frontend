b855f86938c812f22cdacfc453e777bca53a3faa
nidhal <nidhalben13@gmail.com>
2024-12-02 14:57:19 +0100

    home page almost done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 export const routes: Routes = [
     {path:'login',component:LoginComponent},
     { path: '', redirectTo: '/login', pathMatch: 'full' },
-    {path:'signup',component:SignupComponent}
+    {path:'signup',component:SignupComponent},
+    {path:'home',component:HomePageComponent}
 ];
