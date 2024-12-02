bbec87a3cae4102357e788dc1b797c8886137a3a
nidhal <nidhalben13@gmail.com>
2024-12-02 16:57:51 +0100

    crud interfaces

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,16 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { CoursesListComponent } from './courses/courses-list/courses-list.component';
+import { AddCourseComponent } from './courses/add-course/add-course.component';
+import { UsersListComponent } from './users/users-list/users-list.component';
 
 export const routes: Routes = [
     {path:'login',component:LoginComponent},
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     {path:'signup',component:SignupComponent},
-    {path:'home',component:HomePageComponent}
+    {path:'home',component:HomePageComponent},
+    {path:'ManageCourses',component:CoursesListComponent},
+    {path:'AddCourse',component:AddCourseComponent},
+    {path:'ManageUsers',component:UsersListComponent}
 ];
---
+++ src/app/courses/courses-list/courses-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-courses-list',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './courses-list.component.html',
+  styleUrl: './courses-list.component.css'
+})
+export class CoursesListComponent {
+
+}
