a4e3e4dccb1c49dd628b728fcbce6efc17971283
arfaouibadi <arfaouibadi19@gmail.com>
2024-12-02 22:11:48 +0100

    merged user homepage course

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { SignUpComponent } from './auth/signup/signup.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { CoursesListComponent } from './courses/courses-list/courses-list.component';
+import { AddCourseComponent } from './courses/add-course/add-course.component';
+import { UsersListComponent } from './users/users-list/users-list.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'signup', component: SignUpComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: 'ManageCourses', component: CoursesListComponent },
+  { path: 'AddCourse', component: AddCourseComponent },
+  { path: 'ManageUsers', component: UsersListComponent },
 ];
